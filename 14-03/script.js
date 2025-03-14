0c5a481e61cb82f318274a8b5f5f089180a75a49
Rafitz <rafaelfocaca@gmail.com>
2025-03-14 11:04:55 -0300

    Learning Javascript

---
+++ 14-03/script.js
@@ -0,0 +1,21 @@
+let a = 10;
+b = a ** 2;
+
+console.log("Valor de B:", a, "elevado a 2 =", b)
+alert("Valor de b: " + a + " elevado a 2 =" + b)
+
+
+function f1(){
+    let a = 2222;
+}
+
+
+function f2(){
+    let a = 1000;
+}
+
+const PI = 3.14;
+
+// Pesquisar document.getElementById
+
+document.getElementById()
