c98e9e90e0b98d149538582d09dccd93a8ec11e2
rafitz <rafaelfocaca@gmail.com>
2025-03-14 19:09:47 -0300

    Homework: how to use getElementById (basic)

--- 14-03/script.js
+++ 14-03/script.js
@@ -1,21 +1,22 @@
 let a = 10;
 b = a ** 2;
 
-console.log("Valor de B:", a, "elevado a 2 =", b)
-alert("Valor de b: " + a + " elevado a 2 =" + b)
+// console.log("Valor de B:", a, "elevado a 2 =", b)
+// alert("Valor de b: " + a + " elevado a 2 =" + b)
 
 
-function f1(){
-    let a = 2222;
-}
+// function f1(){
+//     let a = 2222;
+// }
 
+// function f2(){
+//     let a = 1000;
+// }
 
-function f2(){
-    let a = 1000;
-}
-
-const PI = 3.14;
+// const PI = 3.14;
 
 // Pesquisar document.getElementById
 
-document.getElementById()
+// Lição de casa 
+document.getElementById("resultado").innerHTML = "<h1>Valor de B: " + a + " elevado a 2 = " + b + "</h1>";
+document.getElementById("resultado").innerHTML = `<h1>Valor de B: ${a} elevado a 2 = ${b}</h1>`;
