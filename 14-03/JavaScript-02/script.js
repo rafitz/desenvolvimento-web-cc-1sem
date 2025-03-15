a1a371b847e0a130133e6ecb38c5bd9601865cb7
rafitz <rafaelfocaca@gmail.com>
2025-03-15 16:53:02 -0300

    Learning how return works

--- 14-03/JavaScript-02/script.js
+++ 14-03/JavaScript-02/script.js
@@ -1,15 +1,27 @@
+// Altera o nome do Bem vindo do HTML
 // let name = prompt("Qual s   eu nome?") 
 // document.getElementById("name").innerHTML = `<h1>Bem-vindo ${name} </h1> `;
 
-function media(n1, n2){
-    var nota1 = n1;
-    var nota2 = n2;
-    var media = (nota1 + nota2) / 2
 
-    return media
+// Faz a media da nota
+// function media(n1, n2){
+//     var nota1 = n1;
+//     var nota2 = n2;
+//     var media = (nota1 + nota2) / 2
+
+//     return media
+// }
+
+// var resultado1 = media(10,8); 
+// var resultado2 = media(6,9)
+
+// console.log(resultado1 + " e " + resultado2)
+
+// Como o Return funciona:
+function saudacao(){
+    return "Ola mundo"
 }
 
-var resultado1 = media(10,8); 
-var resultado2 = media(6,9)
+var s = saudacao()
 
-console.log(resultado1 + " e " + resultado2)
+console.log(s)
